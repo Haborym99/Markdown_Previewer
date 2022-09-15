c4531a36b34cad8001eb35993eaad45d1960c58c
Haborym99 <tristan.collon@outlook.com>
2022-09-15 20:31:30 +0200

    Test passed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { marked } from "marked";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const basicText = `# Demonstration text
+## This text is just here to give a first impression
+In markdown we can have **inline code** (and bold text) at any time like this \` printf(Hello World) \`
+
+We can have link like my [Github page!](https://github.com/Haborym99)
+
+\`\`\`c
+ //Here's a code block
+ 
+{
+ function how_are_you(answer){
+  if(answer == fine){
+    printf("Glad to hear this");
+  }
+  else{
+    printf("Tell me more")
+  }
+ }
 }
+\`\`\`
 
+We can list things:
+- First thing
+- Second thing
+- A third one?
+
+> You like quote?
+> Because you can write some!
+
+
+And of course we can add images and/or GIFs!
+
+![Rick rolling](https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif)
+`;
+
+const html = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
+
+marked.setOptions({breaks : true})
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: basicText,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = (change) => {
+    this.setState({
+      markdown: change.target.value,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div id="editorWrapper">
+          <textarea
+            id="editor"
+            onChange={this.handleChange}
+            value={this.state.markdown}
+          />
+        </div>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+        ></div>
+      </div>
+    );
+  }
+}
 export default App;
